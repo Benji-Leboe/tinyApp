8c9a6e74a05046efa62e4b8e1af6915368c5bf13
Benji-Leboe <benjamin.r.leboe@gmail.com>
2018-09-07 01:34:41 +0000

    add login POST

--- express-server.js
+++ express-server.js
@@ -89,8 +89,8 @@ app.get('/u/:shortURL', (req, res) => {
 
 //POST
 
+//get longURL and append to DB with random gen key
 app.post('/urls', (req, res) => {
-  //get longURL and append to DB with random gen key
   let urlString = "";
   let check = new RegExp('http');
   //check if longURL is http format
@@ -104,8 +104,8 @@ app.post('/urls', (req, res) => {
   res.redirect(`/urls/${randomString}`);
 });
 
+//edit destination URL
 app.post('/urls/:id', (req, res) => {
-  //edit destination URL
   let newURL = "";
   //check if longURL is http format
   let check = new RegExp('http');
@@ -119,24 +119,26 @@ app.post('/urls/:id', (req, res) => {
   res.redirect('/urls');
 });
 
+//delete URL
 app.post('/urls/:id/delete', (req, res) => {
-  //delete URL
+  
   let urlID = req.params.id;
   delete urlDB[urlID];
   res.redirect('/urls');
 });
 
-//User DB and POST register
+//User DB
 let userDB = {
   "userID": {id: 'userID', username: 'example', passwordHash: 'hashedPword'}
 };
 
+//user registration
 app.post('/register', (req, res) => {
   let username = req.body.userLogin;
   let password = req.body.password;
   let passwordCheck = req.body.confirmPassword;
   //check for errors
-  
+  //check for existing user
   for(let user in userDB) {
     if(user.username === username){
       console.log("DB username:", user, "Check value:", username);
@@ -145,6 +147,7 @@ app.post('/register', (req, res) => {
       res.render('pages/register', {errors: errors, username: undefined});
     }  
   };
+  //check for proper username and password format
   if(username.length < 5){
     console.log("Username must be at least 5 characters!");
     errors = 'Username must be at least 5 characters!';
@@ -161,12 +164,14 @@ app.post('/register', (req, res) => {
   }else{
     errors = undefined;
     //handle POST request
+    //hash password
     let passwordHash = bcrypt.genSaltSync(10, (err, salt) => {
       bcrypt.hash(password, salt, (err, hash) => {
         return hash;
       });
     });
     let id = generateRandomString();
+    //append user id, username and password hash to userDB
     userDB[id] = {id, username, passwordHash};
     console.log(userDB);
     req.session.user_id = id;
@@ -178,18 +183,38 @@ app.post('/register', (req, res) => {
 app.post('/login', (req, res) => {
   //login to site
   let username = req.body.userLogin;
+  let password = req.body.password;
+  let hashCheck = undefined;
+  //check username length
+  if(username.length < 1){
+    res.statusCode = 400;
+    errors = 'Please enter a username.';
+    res.render('/login', {errors: errors});
+  }
   //check against existing username in userDB 
-  for(let user in userDB) {
-    if(username.length < 1){
-      res.statusCode = 400;
-      res.send('Please enter a username.')
-    }else if(username !== user){
-      res.statusCode = 400;
-      res.send('Username does not exist!');
-    }
-  };
-  res.session = username;
-  res.redirect('/');
+  if(hashCheck === undefined){
+    for(let user in userDB) {
+      if(!user.hasOwnProperty(username)){
+        errors = 'Invalid username or password.';
+        res.statusCode = 400;
+        res.render('/login', {errors: errors});
+      }else{
+        errors = undefined;
+        hashCheck = bcrypt.compareSync(password, user.passwordHash, (err, res) => {
+          return res;
+        });
+      }
+    };
+  }
+
+  if(hashCheck === false){
+    res.statusCode = 400;
+    errors = 'Invalid username or password.';
+    res.render('/login', {errors: errors});
+  }else{
+    req.session.user_id = id;
+    res.redirect('/urls');
+  }
 });
 
 app.post('/logout', (req, res) => {
